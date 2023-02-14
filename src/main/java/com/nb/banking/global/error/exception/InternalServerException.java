61fa497c83247def6b8f541ca57317ef14316188
hanjongho <lgwl81@gmail.com>
2023-02-14 16:47:28 +0900

    프로젝트 생성 및 필요 템플릿 추가

---
+++ src/main/java/com/nb/banking/global/error/exception/InternalServerException.java
@@ -0,0 +1,26 @@
+package com.nb.banking.global.error.exception;
+
+import com.nb.banking.global.error.ErrorCode;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class InternalServerException extends BusinessException {
+    private String message;
+
+    public InternalServerException(String message) {
+        super(ErrorCode._BAD_REQUEST);
+        this.message = message;
+    }
+
+    public InternalServerException(ErrorCode errorCode, String message) {
+        super(errorCode);
+        this.message = message;
+    }
+
+    public InternalServerException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
---
+++ src/main/java/com/nb/banking/global/error/exception/UnauthorizedException.java
@@ -0,0 +1,26 @@
+package com.nb.banking.global.error.exception;
+
+import com.nb.banking.global.error.ErrorCode;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class UnauthorizedException extends BusinessException {
+    private String message;
+
+    public UnauthorizedException(String message) {
+        super(ErrorCode._BAD_REQUEST);
+        this.message = message;
+    }
+
+    public UnauthorizedException(ErrorCode errorCode, String message) {
+        super(errorCode);
+        this.message = message;
+    }
+
+    public UnauthorizedException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
