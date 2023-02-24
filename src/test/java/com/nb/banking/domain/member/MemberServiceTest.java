8cf5d4c94e9d6d1c09d3c5bf5a8e80c2d5e3cdc7
Jongho Han <lgwl81@gmail.com>
2023-02-25 03:57:24 +0900

    테스트 코드 수정 및 불 필요한 코드 제거 (#6)

--- src/test/java/com/nb/banking/domain/member/MemberServiceTest.java
+++ src/test/java/com/nb/banking/domain/member/MemberServiceTest.java
@@ -6,13 +6,19 @@ import java.util.Set;
 
 import javax.transaction.Transactional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
 
+import com.nb.banking.domain.member.dto.MemberDto;
 import com.nb.banking.domain.member.entity.Member;
 import com.nb.banking.global.error.exception.BadRequestException;
 
@@ -25,6 +31,20 @@ class MemberServiceTest {
 	@Autowired
 	private MemberService memberService;
 
+	@Autowired
+	private MemberRepository memberRepository;
+
+	private TransactionTemplate transaction;
+
+	@Autowired
+	private PlatformTransactionManager tm;
+
+	@BeforeEach
+	void setUp() {
+		transaction = new TransactionTemplate(tm);
+		transaction.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+	}
+
 	@Nested
 	@DisplayName("회원가입")
 	class join {
@@ -36,13 +56,18 @@ class MemberServiceTest {
 			String loginId = "Jongho";
 			String password = "1234";
 
+			MemberDto memberDto = MemberDto.builder()
+					.loginId(loginId)
+					.password(password)
+					.amount(1000L)
+					.build();
+
 			//when
-			Member member = memberService.join(loginId, password, 0L);
+			Member member = memberService.join(memberDto);
 
 			//then
 			assertNotNull(member.getAccount());
 			assertEquals(member.getLoginId(), loginId);
-			assertEquals(member.getPassword(), password);
 		}
 
 		@Test
@@ -52,11 +77,17 @@ class MemberServiceTest {
 			String loginId = "Jongho";
 			String password = "1234";
 
+			MemberDto memberDto = MemberDto.builder()
+					.loginId(loginId)
+					.password(password)
+					.amount(1000L)
+					.build();
+
 			//when
-			memberService.join(loginId, password, 0L);
+			memberService.join(memberDto);
 
 			//then
-			assertThrows(BadRequestException.class, () -> memberService.join(loginId, password, 0L));
+			assertThrows(BadRequestException.class, () -> memberService.join(memberDto));
 		}
 
 	}
@@ -69,17 +100,37 @@ class MemberServiceTest {
 		@DisplayName("성공")
 		void success() throws Exception {
 			//given
-			String myId = "Jongho";
+			String myId = "Jongho1";
 			String myPassword = "1234";
-			Member me = memberService.join(myId, myPassword, 0L);
+
+			MemberDto memberDto = MemberDto.builder()
+					.loginId(myId)
+					.password(myPassword)
+					.amount(1000L)
+					.build();
+
+			transaction.execute((status -> memberService.join(memberDto)));
 
 			String friendId1 = "JonghoFriend1";
 			String friendPassword1 = "5678";
-			Member friend1 = memberService.join(friendId1, friendPassword1, 0L);
+
+			MemberDto friend1Dto = MemberDto.builder()
+					.loginId(friendId1)
+					.password(friendPassword1)
+					.amount(1000L)
+					.build();
+
+			transaction.execute((status -> memberService.join(friend1Dto)));
 
 			String friendId2 = "JonghoFriend2";
 			String friendPassword2 = "8765";
-			Member friend2 = memberService.join(friendId2, friendPassword2, 0L);
+
+			MemberDto friend2Dto = MemberDto.builder()
+					.loginId(friendId2)
+					.password(friendPassword2)
+					.amount(1000L)
+					.build();
+			transaction.execute((status -> memberService.join(friend2Dto)));
 
 			memberService.addConnection(myId, friendId1);
 			memberService.addConnection(myId, friendId2);
@@ -88,6 +139,9 @@ class MemberServiceTest {
 			Set<Member> friendList = memberService.findAllConnectedMember(myId);
 
 			//then
+			Member me = memberRepository.findByLoginId(myId).get();
+			Member friend1 = memberRepository.findByLoginId(friendId1).get();
+			Member friend2 = memberRepository.findByLoginId(friendId2).get();
 			assertAll(
 					() -> assertEquals(2, friendList.size()),
 					() -> assertTrue(me.getFriendList().contains(friend1)),
@@ -107,18 +161,34 @@ class MemberServiceTest {
 		@DisplayName("성공")
 		void success() throws Exception {
 			//given
-			String myId = "Jongho";
+			String myId = "Jonghozzang";
 			String myPassword = "1234";
-			Member me = memberService.join(myId, myPassword, 0L);
 
-			String friendId = "JonghoFriend";
+			MemberDto memberDto = MemberDto.builder()
+					.loginId(myId)
+					.password(myPassword)
+					.amount(1000L)
+					.build();
+
+			transaction.execute((status -> memberService.join(memberDto)));
+
+			String friendId = "JonghoFriend5555";
 			String friendPassword = "5678";
-			Member you = memberService.join(friendId, friendPassword, 0L);
+
+			MemberDto friendDto = MemberDto.builder()
+					.loginId(friendId)
+					.password(friendPassword)
+					.amount(1000L)
+					.build();
+
+			transaction.execute((status -> memberService.join(friendDto)));
 
 			//when
 			memberService.addConnection(myId, friendId);
 
 			//then
+			Member me = memberRepository.findByLoginId(myId).get();
+			Member you = memberRepository.findByLoginId(friendId).get();
 			assertAll(
 					() -> assertEquals(1, me.getFriendList().size()),
 					() -> assertEquals(1, you.getFriendList().size()),
@@ -131,16 +201,27 @@ class MemberServiceTest {
 		@DisplayName("실패 - 이미 등록된 친구 일 때")
 		void fail() throws Exception {
 			//given
-			String myId = "Jongho";
+			String myId = "Jongho111111";
 			String myPassword = "1234";
-			memberService.join(myId, myPassword, 0L);
 
-			String friendId = "JonghoFriend";
+			MemberDto memberDto = MemberDto.builder()
+					.loginId(myId)
+					.password(myPassword)
+					.amount(1000L)
+					.build();
+			transaction.execute((status -> memberService.join(memberDto)));
+
+			String friendId = "JonghoFriend111111";
 			String friendPassword = "5678";
-			memberService.join(friendId, friendPassword, 0L);
+
+			MemberDto friendDto = MemberDto.builder()
+					.loginId(friendId)
+					.password(friendPassword)
+					.amount(1000L)
+					.build();
+			transaction.execute((status -> memberService.join(friendDto)));
 
 			memberService.addConnection(myId, friendId);
-
 			//when
 			assertThrows(BadRequestException.class, () -> memberService.addConnection(myId, friendId));
 		}
